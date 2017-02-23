c67c52d68432ad648553073379b722a49805a192
Nuno Guedelha <nuno.guedelha@iit.it>
2017-02-23 08:00:30 +0100

    Renamed CalibValidation module to class SensorDiagnosis. Completed main app interface with inertial sensors calibration parameters.

---
+++ src/@SensorDiagnosis/SensorDiagnosis.m
@@ -0,0 +1,34 @@
+classdef SensorDiagnosis
+    %SensorDiagnosis Holds all methods for run a diagnosis on sensor data
+    %   'runDiagnosis()' is the main procedure for checking the inertial
+    %   sensors data and plotting the key performance indicators related to
+    %   the sensors calibration.
+    
+    methods(Static = true, Access = public)
+        runDiagnosis(...
+            modelPath,calibrationMap,...
+            calibedParts,calibedJointsIdxes,dataPath,...
+            measedSensorList,measedPartsList);
+    end
+    
+    methods(Static = true, Access = protected)
+        checkAccelerometersAnysotropy(...
+            data_bc,data_ac,sensorsIdxListFile,...
+            sensMeasCell_bc,sensMeasCell_ac,...
+            figsFolder,logTest,loadJointPos);
+        
+        [pVec,dVec,dOrient,d] = ellipsoid_proj_distance_fromExp(x,y,z,centre,radii,R);
+        
+        checkSensorMeasVsEst(...
+            data,sensorsIdxListFile,...
+            sensMeasCell,sensEstCell,...
+            figsFolder,logTest,logtag);
+        
+        plotFittingEllipse(centre,radii,R,sensMeasCell);
+        
+        plotJointTrajectories(data,ModelParams,figsFolder,logTest);
+        
+        h = plotNprintDistrb(FID,dOrient,fitGaussian,color,alpha,prevh,axbc,axac);
+    end
+    
+end
--- src/conf/sensorSelfCalibratorInit.m
+++ src/conf/sensorSelfCalibratorInit.m
@@ -10,13 +10,13 @@ modelPath = '../models/iCubGenova05/iCubFull.urdf';
 calibrationMapFile = 'calibrationMap.mat';
 saveCalibration = false;
 
+
 %% Standard or custom calibration
 calibrationType = 'standard';
 
 %% standard calibration tasks checklist
 runDiagnosis = false;
 calibrateAccelerometers = false;
-calibrateIMU = false;
 calibrateJointEncoders = true;
 calibrateFTsensors = false;
 calibrateGyroscopes = false;
@@ -35,14 +35,80 @@ calibrateGyroscopes = false;
 %     'diagCmp',{
 %     };
 
-%% Accelerometers/IMU gains/offsets calibration
+
+%% Sensors diagnosis (only accelerometers for now)
+
+% define the robot limb holding the sensors on which we run the diagnosis.
+diagnosedParts = {'left_leg'};
+
+% Fine selection of the accelerometers:
+% Select (set to 'true') the accelerometers to analyse through the respective indexes.
+% 
+% (Part)           (inertial MTB boards)
+% 
+%                  (upper)       (forearm)      (hand)
+% left_arm :       1b10..1b13    1b8..1b9       1b7
+% right_arm:       2b10..2b13    2b8..2b9       2b7
+% mtbSensorAct:      10....13      8....9         7
+%                  (upper)       (lower)        (foot)
+% left_leg :       10b1..10b7    10b8..10b11    10b12,10b13
+% right_leg:       11b1..11b7    11b8..11b11    11b12,11b13
+% mtbSensorAct idx:   1.....7       8.....11       12,   13
+% 
+% head     :       head_imu 3D accelerometer
+% mtbSensorAct idx:  1
+% 
+% torso    :       9b7..9b10
+% mtbSensorAct idx:  7....10
+% 
+% some sensors are de-activated because of faulty behaviour
+mtbSensorAct.left_arm = [10:13 8:9 7];
+mtbSensorAct.right_arm = [10:13 8:9 7];
+mtbSensorAct.left_leg = 1:13;
+mtbSensorAct.right_leg = 1:13;
+mtbSensorAct.torso = 7:10;
+mtbSensorAct.head = 1;
+
+% Save generated figures: if this flag is set to true, all data is saved and figures 
+% printed in a new folder indexed by a unique iteration number. Log
+% classification information are saved in text format for easier search from
+% a file explorer.
+logTest = true;
+
+% Sensor data acquisition: ['new'|'last'|<id>]
+sensorDataAcq = {'new'};
+
+% wrap parameters
+sensorDiagnosis = struct('diagnosedParts',{diagnosedParts},'mtbSensorAct',mtbSensorAct,'logTest',logTest,'sensorDataAcq',{sensorDataAcq});
+clear diagnosedParts mtbSensorAct logTest sensorDataAcq;
+
+%% MTB/IMU accelerometers gains/offsets calibration
+
+% Calibrated parts:
+% Only the accelerometers from these parts (limbs) will be calibrated
+calibedParts = {'left_leg','right_leg'};
+
+% some sensors are de-activated because of faulty behaviour
+mtbSensorAct.left_arm = [10:13 8:9 7];
+mtbSensorAct.right_arm = [10:13 8:9 7];
+mtbSensorAct.left_leg = 1:13;
+mtbSensorAct.right_leg = 1:13;
+mtbSensorAct.torso = 7:10;
+mtbSensorAct.head = 1;
+
+% Sensor data acquisition: ['new'|'last'|<id>]
+sensorDataAcq = {'new'};
+
+% wrap parameters
+accelerometersCalib = struct('calibedParts',{calibedParts},'mtbSensorAct',mtbSensorAct,'sensorDataAcq',{sensorDataAcq});
+clear calibedParts mtbSensorAct sensorDataAcq;
 
 
 %% Joint encoders offsets calibration
 
 % Calibrated parts:
 % Only the joint encoders from these parts (limbs) will be calibrated
-calibedParts = {'left_leg','torso','right_leg'};
+calibedParts = {'left_leg','right_leg'};
 
 % Fine selection of joint encoders:
 % Select the joints to calibrate through the respective indexes. These indexes match 
@@ -73,9 +139,14 @@ calibedJointsIdxes.torso = 0:2;
 calibedJointsIdxes.head = 0:2;
 
 % Sensor data acquisition: ['new'|'last'|<id>]
-sensorDataAcq = 'new';
+sensorDataAcq = {'calibrator',7};
 
 % wrap parameters
 jointEncodersCalib = struct('calibedParts',{calibedParts},'calibedJointsIdxes',calibedJointsIdxes,'sensorDataAcq',{sensorDataAcq});
 clear calibedParts calibedJointsIdxes sensorDataAcq;
+
+%% FT sensors gains/offsets calibration (TBD)
+
+ftSensorsCalib = struct();
+gyroscopesCalib = struct();
 
