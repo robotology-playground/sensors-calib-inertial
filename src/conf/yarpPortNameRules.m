25fd8263b4663fb75d88e15455084f8e1f8c251f
Nuno Guedelha <nuno.guedelha@iit.it>
2018-02-02 09:58:35 +0100

    Added motion profile with gyroscopes logging for ang. acc. computation

--- src/conf/yarpPortNameRules.m
+++ src/conf/yarpPortNameRules.m
@@ -16,3 +16,15 @@ imuSensors_folder_rule_dumper =  '[datapath ''/'' part ''/inertial'']';
 FTSensors_port_rule_icub   =        '[''/'' robotname ''/'' part ''/analog:o'']';
 FTSensors_port_rule_dumper = '[''/dumper/'' robotname ''/'' part ''/analog:i'']';
 FTSensors_folder_rule_dumper =             '[datapath ''/'' part ''/analog:o'']';
+
+xsensSensors_port_rule_icub   =        '[''/'' robotname ''/xsens_inertial'']';
+xsensSensors_port_rule_dumper = '[''/dumper/'' robotname ''/xsens_inertial'']';
+xsensSensors_folder_rule_dumper =  '[datapath ''/'' part ''/xsens_inertial'']';
+
+gyro8Sensors_port_rule_icub   =        '[''/'' robotname ''/'' part ''/inertialEMS8'']';
+gyro8Sensors_port_rule_dumper = '[''/dumper/'' robotname ''/'' part ''/inertialEMS8'']';
+gyro8Sensors_folder_rule_dumper =             '[datapath ''/'' part ''/inertialEMS8'']';
+
+gyro9Sensors_port_rule_icub   =        '[''/'' robotname ''/'' part ''/inertialEMS9'']';
+gyro9Sensors_port_rule_dumper = '[''/dumper/'' robotname ''/'' part ''/inertialEMS9'']';
+gyro9Sensors_folder_rule_dumper =             '[datapath ''/'' part ''/inertialEMS9'']';
