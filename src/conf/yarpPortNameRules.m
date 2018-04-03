0973e622e8e5eb9700160171ee3e5033eeb4ec57
Francisco <ing.andrade.francisco@gmail.com>
2018-04-03 18:02:03 +0200

    drop the :o endings for the logging and added a little command that needs to be runned in my computer for matlab to recognize the right libraries to use yarpdatadumper

--- src/conf/yarpPortNameRules.m
+++ src/conf/yarpPortNameRules.m
@@ -2,8 +2,8 @@
 % (accelerometers, gyroscopes, IMUs and FT sensors) data is published.
 
 joints_port_rule_icub   =        '[''/'' robotname ''/'' part ''/stateExt:o'']';
-joints_port_rule_dumper = '[''/dumper/'' robotname ''/'' part ''/stateExt:i'']';
-joints_folder_rule_dumper =             '[datapath ''/'' part ''/stateExt:i'']';
+joints_port_rule_dumper = '[''/dumper/'' robotname ''/'' part ''/stateExt'']';
+joints_folder_rule_dumper =             '[datapath ''/'' part ''/stateExt'']';
 
 accSensors_port_rule_icub   =        '[''/'' robotname ''/'' part ''/inertialMTB'']';
 accSensors_port_rule_dumper = '[''/dumper/'' robotname ''/'' part ''/inertialMTB'']';
@@ -14,5 +14,5 @@ imuSensors_port_rule_dumper = '[''/dumper/'' robotname ''/inertial'']';
 imuSensors_folder_rule_dumper =  '[datapath ''/'' part ''/inertial'']';
 
 FTSensors_port_rule_icub   =        '[''/'' robotname ''/'' part ''/analog:o'']';
-FTSensors_port_rule_dumper = '[''/dumper/'' robotname ''/'' part ''/analog:o'']';
-FTSensors_folder_rule_dumper =             '[datapath ''/'' part ''/analog:o'']';
+FTSensors_port_rule_dumper = '[''/dumper/'' robotname ''/'' part ''/analog'']';
+FTSensors_folder_rule_dumper =             '[datapath ''/'' part ''/analog'']';
