80181cc8f5b5abbe5e70ea50c9ccb53762eb9bc4
Nuno Guedelha <nuno.guedelha@iit.it>
2019-01-08 16:12:49 +0100

    Improved savefigures2: png format with tight margins (inset) and l=2*h.

--- src/+UI/saveFigures2.m
+++ src/+UI/saveFigures2.m
@@ -9,6 +9,16 @@ for figNum = figureNums(:)'
     figH = figure(figNum);
     set(figH,'PaperPositionMode','auto');
     set(gca,'FontSize',45);
+    % choose dimensions
+    figH.PaperPosition(3) = 2*figH.PaperPosition(4);
+    % reduce extra white spaces
+    ax = gca;
+    outerpos = ax.OuterPosition;
+    ti = ax.TightInset;
+    left = outerpos(1) + ti(1);
+    ax_width = outerpos(3) - ti(1) - ti(3);
+    ax.Position([1,3]) = [left ax_width];
+    % save figure
     filename = strrep(figH.UserData,'\_','_');
     savefig(figH,[folderName '/' filename '.fig']);
     print('-dpng','-r150','-opengl',[folderName '/' filename]);
